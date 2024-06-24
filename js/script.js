eec4b0bba46979da6cb918ed1005417eaf5c4502
Kaio <kakacnmendes@gmail.com>
2024-06-24 09:38:45 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
 const urlApi = "http://localhost:3000"
 
-const loadingElement = document.querySelector("#loading")
-
-// * pegar todos os produtos ja cadastrados
-
-async function getProducts() {
-    
-    const response = await fetch(urlApi);
-
-    console.log(response);
-
-    const data = await response.json();
-
-    console.log(data);
-
-    loadingElement.classList.add("hide");
-
-    data.map((product) => {
-
-        const div = document.createElement("div")
-        const title = document.createElement("h2")
-        const body = document.createElement("p")
-        const link = document.createElement("a")
-
-    }) 
-
-}
-
-getProducts();
+// * GET
