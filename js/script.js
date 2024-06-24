8903f05d194ba1611f014f253c08f899a88ef1fd
Kaio <kakacnmendes@gmail.com>
2024-06-24 10:16:50 -0300

    add get

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-const urlApi = "http://localhost:3000"
+const urlApi = "http://localhost:3000/produtos"
 
 // * GET
+
+async function carregarUsuarios() {
+    const requestMethod = {
+        method: "GET"
+    };
+
+    const response = await fetch(urlApi, requestMethod)
+    const produtos = await response.json();
+
+    alimentarCards(produtos);
+
+}
+
+function alimentarCards(produtos) {
+    const htmlCards = produtos.map(item =>
+        
+        `
+        <div class="card">
+            <img src="${item.image}" alt="${item.name}">
+            <h3>${item.name}</h3>
+            <p>${item.descricao}</p>
+            <h4>${item.price}</h4>
+        </div>
+        `
+    )
+
+    const htmlConteudo = htmlCards.join("")
+    document.getElementById('card').innerHTML = htmlConteudo
+}
+
+// Call the function to load users when the page loads
+document.addEventListener('DOMContentLoaded', carregarUsuarios);
