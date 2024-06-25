c11a99623560b8f551ba91fa0dd0236768ff1967
Kaio <kakacnmendes@gmail.com>
2024-06-25 11:24:31 -0300

    POST request done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const { title } = require("process");
+
 const urlApi = "http://localhost:3000/produtos"
 
 // * GET
@@ -31,5 +33,57 @@ function alimentarCards(produtos) {
     document.getElementById('card').innerHTML = htmlConteudo
 }
 
+async function salvarProduto() {
+    const nomeProduto = document.getElementById('nome').value
+    const precoProduto = document.getElementById('preco').value
+    const descProduto = document.getElementById('descricao').value
+    const image = document.getElementById("image").files[0]
+
+    // ! tratamento de erro para campo vazio
+
+    if (!nomeProduto) {
+        Swal.fire({
+            title: "Por favor, insira um nome para o produto",
+            icon: "error"
+        })
+        return;
+    }
+
+    if (!precoProduto) {
+        Swal.fire({
+            title: "Por favor, insira um preço para o produto",
+            icon: "error"
+        })
+        return;
+    }
+
+    let reader = new FileReader()
+    reader.onload = async function() {
+        const imageBase64 = reader.result
+
+        const payload = {
+            nome: nomeProduto,
+            preco: precoProduto,
+            descricao: descProduto,
+            image: imageBase64
+        }
+        const requestMethod = {
+            method: "POST",
+            body: JSON.stringify(payload)
+        };
+
+        await fetch(urlApi, requestMethod)
+
+        const modal = document.getElementById('staticBackdrop');
+        bootstrap.Modal.getInstance(modal).hide()
+
+        carregarUsuarios()
+    }
+
+    reader.readAsDataURL(image)
+
+}
+
 // Call the function to load users when the page loads
 document.addEventListener('DOMContentLoaded', carregarUsuarios);
+document.getElementById('btn-salvarProduto').addEventListener('click', salvarProduto)
